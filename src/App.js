7836ee58d8bf826c5345731f82c8792b830d0531
Daniel Acevedo Lopez <93848188+acevedod1974@users.noreply.github.com>
2024-11-23 14:39:08 -0400

    Update routing in App.js to use Routes instead of Switch for React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ProcesosDeFabricacion1 from "./pages/ProcesosDeFabricacion1";
 import ProcesosDeFabricacion2 from "./pages/ProcesosDeFabricacion2";
@@ -7,11 +7,11 @@ import ProcesosDeFabricacion2 from "./pages/ProcesosDeFabricacion2";
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/pf1" component={ProcesosDeFabricacion1} />
-        <Route path="/pf2" component={ProcesosDeFabricacion2} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/pf1" element={<ProcesosDeFabricacion1 />} />
+        <Route path="/pf2" element={<ProcesosDeFabricacion2 />} />
+      </Routes>
     </Router>
   );
 };
